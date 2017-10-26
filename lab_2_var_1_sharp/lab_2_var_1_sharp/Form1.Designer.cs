fbf85332b77212ca41cc7a8f314b0df1f9ad29e5
TheSilenceOfMind <baladkirill@mail.ru>
2017-10-26 14:17:16 +0300

    lab2 is done

---
+++ lab_2_var_1_sharp/lab_2_var_1_sharp/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace lab_2_var_1_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBMPFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.swapRAndBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.actionsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(65, 484);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openBMPFileToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(85, 19);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openBMPFileToolStripMenuItem
+            // 
+            this.openBMPFileToolStripMenuItem.Name = "openBMPFileToolStripMenuItem";
+            this.openBMPFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openBMPFileToolStripMenuItem.Text = "Open BMP file";
+            this.openBMPFileToolStripMenuItem.Click += new System.EventHandler(this.openBMPFileToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As ...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.swapRAndBToolStripMenuItem});
+            this.actionsToolStripMenuItem.Enabled = false;
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(85, 19);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // swapRAndBToolStripMenuItem
+            // 
+            this.swapRAndBToolStripMenuItem.Name = "swapRAndBToolStripMenuItem";
+            this.swapRAndBToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.swapRAndBToolStripMenuItem.Text = "Swap R and B";
+            this.swapRAndBToolStripMenuItem.Click += new System.EventHandler(this.swapRAndBToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(65, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(947, 484);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(85, 19);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1012, 484);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Вариант №1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBMPFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem swapRAndBToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
+
---
+++ lab_2_var_1_sharp/lab_2_var_1_sharp/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2_var_1_sharp
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bmp = (Bitmap)pictureBox1.Image;
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format24bppRgb);
+            using (Graphics gr = Graphics.FromImage(newBmp))
+            {
+                gr.DrawImage(bmp, 0, 0, bmp.Width, bmp.Height);
+            }
+
+            SaveFileDialog savedialog = new SaveFileDialog();
+            savedialog.Title = "Save as ...";
+            savedialog.OverwritePrompt = true;
+            savedialog.CheckPathExists = true;
+            savedialog.Filter = "Image Files(*.BMP)|*.BMP";
+            savedialog.ShowHelp = true;
+            if (savedialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    bmp.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+                catch
+                {
+                    MessageBox.Show("Can't save the image", "Error!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void openBMPFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files(*.BMP;)|*.BMP;";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    bmp = new Bitmap(dialog.FileName);
+                    pictureBox1.Image = bmp;
+                    pictureBox1.Invalidate();
+                    actionsToolStripMenuItem.Enabled = true;
+                    saveAsToolStripMenuItem.Enabled = true;
+                }
+                catch
+                {
+                    MessageBox.Show("Can't display selected image!",
+                    "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void swapRAndBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ImageAttributes imageAttributes = new ImageAttributes();
+
+            float[][] colorMatrixElements = {
+                                                new float[] {0, 0, 1, 0, 0}, // R
+                                                new float[] {0, 1, 0, 0, 0}, // G
+                                                new float[] {1, 0, 0, 0, 0}, // B
+                                                new float[] {0, 0, 0, 1, 0}, // Aplha
+                                                new float[] {0, 0, 0, 0, 1}  // w
+                                            };
+
+            ColorMatrix colorMatrix = new ColorMatrix(colorMatrixElements);
+
+            imageAttributes.SetColorMatrix(colorMatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.DrawImage(bmp, new Rectangle(0, 0, bmp.Size.Width, bmp.Size.Height), 0, 0, bmp.Size.Width, bmp.Size.Height, GraphicsUnit.Pixel, imageAttributes);
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
