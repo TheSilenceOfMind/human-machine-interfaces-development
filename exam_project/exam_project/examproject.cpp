9e09874b4a7ad398059f49faece1a23f1a579898
TheSilenceOfMind <baladkirill@mail.ru>
2017-11-15 21:29:46 +0300

    removed useless code; added timer; added different cursors on regions

--- exam_project/exam_project/examproject.cpp
+++ exam_project/exam_project/examproject.cpp
@@ -2,21 +2,24 @@
 //
 
 #include "stdafx.h"
-#include "examproject.h"#include <windows.h>
+#include "examproject.h"
+#include <windows.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
+#include <atlbase.h>
 #include "KWnd.h"
 #include "resource.h"
-
-#define Pi 3.14159265
+#include <mmsystem.h>
+#pragma comment( lib, "Winmm.lib" )
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-void DrawPlayer(HWND hwnd, HDC hdc, HDC hMemFrameDC, HBITMAP hBmp, BITMAP bm, FLOAT x, FLOAT y);
-int getIDRegion(POINT p);
+int getIDRegion(int x, int y, BYTE *maskBits, int width);
+void debug(int val);
 
-int cx, cy; // размеры экрана
 BITMAP bm; // инфо о растре
 bool dragging = false; // if left mouse button is pressed right now
+
 
 //====================================================================
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -28,13 +31,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		0, 0,
 		500, 500,
 		CS_HREDRAW | CS_VREDRAW,
-		WS_POPUP | WS_VISIBLE,
+		WS_POPUP,
 		nullptr
 	);
-
-	cx = GetSystemMetrics(SM_CXSCREEN);
-	cy = GetSystemMetrics(SM_CYSCREEN);
-
+	
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
@@ -45,21 +45,66 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 //====================================================================
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	static FLOAT x = 0, y = 0; // текуща€ позици€ окна с м€чом
-	static FLOAT dX, dY; // приращени€ координат дл€ сдвига на новую позицию
-	static int alpha = 0; // угол, определ€ющий вращение м€ча
-
-	static HBITMAP hBmpPlayer; // растр с изображением м€ча
-	static HDC hMemDcFrame;   // совместимый контекст (контекст в пам€ти)
-	static HBITMAP hBmpFrame; // растр дл€ совместимого контекста
-	static HDC hDC;          // контекст диспле€
-	static RECT rect;  // пр€моугольник клиентской области
+	static HBITMAP hBmpPlayer;
+	static HDC hDC;
+	static RECT rect;
 	static POINT MousePnt, curPoint; // used to get coordinates of the mouse
+	static unsigned char *pMaskBits; // used in getIDRegion function
+	static BITMAP bmMask;
+	static BITMAP bm;
+	static HRGN colorReg = 0;
+	const static int PERIOD = 1000 / 120;  // (ms) for color change 
+#define NUM 3
+	const char* musicList[NUM] = { "D:\\Rammstein_Mutter.wav", "D:\\Chuck_Berry_-_Johnny_B_Goode.wav", "D:\\Carlos_Cipa-Lie_with_Me.wav" };
 
-	HDC hMemBmpDcPlayer;
+	static int cntList = 0;
+	static HDC hMemBmpDcPlayer;
+	static bool isPlaying = false;
+	HDC dc;
 
+	static int x, y;
+	static int b = 0;
+
+	if (pMaskBits == nullptr) {
+		// init bits of the mask
+		HBITMAP hMask = (HBITMAP)LoadImage(NULL, L"D:\\mask_ex.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		if (hMask == NULL) {
+			MessageBox(nullptr, L"Error! Loading Image", NULL, NULL);
+		}
+		GetObject(hMask, sizeof(bmMask), &bmMask);
+		pMaskBits = (BYTE *)bmMask.bmBits;
+
+		// init dynamic color region
+		int pixel;
+		int xStart = -1;
+		for (int i = 0; i < bm.bmHeight; i++) {
+			for (int j = 0; j < bm.bmWidth; j++) {
+				pixel = pMaskBits[i*bm.bmWidthBytes + j];
+				if (pixel == 255) {
+					if (xStart == -1) xStart = j;
+				} else {
+					if (xStart != -1) {
+						if (colorReg == 0)
+							colorReg = CreateRectRgn(xStart, bm.bmHeight - i - 1, j, bm.bmHeight - i);/*HRGN CreateRectRgn(
+																								  _In_ int nLeftRect,
+																								  _In_ int nTopRect,
+																								  _In_ int nRightRect,
+																								  _In_ int nBottomRect
+																								  );*/
+						else
+							CombineRgn(colorReg, colorReg, CreateRectRgn(xStart,
+								bm.bmHeight - i - 1, j, bm.bmHeight - i), RGN_OR);
+						xStart = -1;
+					}
+				}
+
+			}
+		}
+	}
+		
 	switch (message) {
 	case WM_CREATE:
+		// init all vars
 		hDC = GetDC(hWnd);
 		GetClientRect(hWnd, &rect);
 		hBmpPlayer = LoadBitmap((HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE), MAKEINTRESOURCE(IDB_BITMAP1));
@@ -67,36 +112,100 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		break;
 
 	case WM_PAINT:
-		hMemBmpDcPlayer = CreateCompatibleDC(hDC);
+		//debug(b++);
+		PAINTSTRUCT ps;
+		dc = BeginPaint(hWnd, &ps);
+		hMemBmpDcPlayer = CreateCompatibleDC(dc);
 		SelectObject(hMemBmpDcPlayer, hBmpPlayer);
-		BitBlt(hDC, 0, 0, bm.bmWidth, bm.bmHeight,
-				hMemBmpDcPlayer, 0, 0, SRCCOPY);
+		BitBlt(dc, 0, 0, bm.bmWidth, bm.bmHeight,
+			hMemBmpDcPlayer, 0, 0, SRCCOPY);
+		EndPaint(hWnd, &ps);
 		break;
 
 	case WM_DESTROY:
 		ReleaseDC(hWnd, hDC);
-		DeleteDC(hMemDcFrame);
 		PostQuitMessage(0);
 		break;
 
 	case WM_LBUTTONDOWN:
-		// TODO : checking with mask
 		GetCursorPos(&MousePnt);
-		switch (getIDRegion(MousePnt)) {
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
+		switch (getIDRegion(x, y, pMaskBits, bm.bmWidth)) {
 		// move window
 		case 0:	
 			dragging = true;
 			SetCapture(hWnd);
+			break;
+
+		// red button - close 
+		case 1: 
+			SendMessage(hWnd, WM_DESTROY, 0, 0);
+			break;
+
+		// green or right - next music
+		case 2:
+			if (isPlaying) {
+				USES_CONVERSION;
+				cntList = (cntList + 1) % NUM;
+				PlaySound(NULL, NULL, SND_ASYNC);
+				LPCWSTR w = A2W(musicList[cntList]);
+				PlaySound(w, NULL, SND_ASYNC | SND_LOOP);
+			}
+			break;
+
+		// blue - play/stop
+		case 3:
+			if (isPlaying) {
+				PlaySound(NULL, NULL, SND_ASYNC);
+				isPlaying = false;
+				//KillTimer(hWnd, 1);
+			} else {
+				USES_CONVERSION;
+				LPCWSTR w = A2W(musicList[cntList]);
+				PlaySound(w, NULL, SND_ASYNC | SND_LOOP);
+				isPlaying = true;
+				SetTimer(hWnd, 1, PERIOD, NULL);
+			}
+			break;
+
+		// left - previous music
+		case 4:
+			if (isPlaying) {
+				if (cntList == 0)
+					cntList = NUM - 1;
+				else
+					cntList -= 1;
+					
+				USES_CONVERSION;
+				PlaySound(NULL, NULL, SND_ASYNC);
+				LPCWSTR w = A2W(musicList[cntList]);
+				PlaySound(w, NULL, SND_ASYNC | SND_LOOP);
+			}
+			break;
+
 		default:
 			MessageBox(NULL, L"Error", L"Error", 0);
 			break;
 		}
-		
-		
+		break;
+
+	case WM_TIMER :
+		/*StretchBlt(hDC, 0, 0, bm.bmWidth, bm.bmHeight,
+			hDC, 0, 0, bm.bmWidth/2, bm.bmHeight/2, SRCCOPY);
+*/
 		break;
 
 	case WM_MOUSEMOVE:
-		if (dragging) {		
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
+		if (getIDRegion(x, y, pMaskBits, bm.bmWidth) != 0) {
+			SetCursor(LoadCursor((HINSTANCE)GetWindowLong(hWnd, GWL_EXSTYLE), MAKEINTRESOURCE(IDC_HAND)));
+		} else {
+			SetCursor(LoadCursor((HINSTANCE)GetWindowLong(hWnd, GWL_EXSTYLE), MAKEINTRESOURCE(IDC_ARROW)));
+		}
+
+		if (dragging) {	
 			GetCursorPos(&curPoint);
 
 			rect.left = rect.left + (curPoint.x - MousePnt.x);
@@ -116,8 +225,37 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		return DefWindowProc(hWnd, message, wParam, lParam);
 		break;
 	}
+	return 0;
 }
 
-int getIDRegion(POINT p) {
-	return 0;
+int getIDRegion(int x, int y, unsigned char* mask, int width) {
+	int ans;
+	unsigned char val = mask[(width-y) * width + x];
+
+	debug(val);
+
+	switch (val) {
+	case 249:
+		ans = 1;
+		break;
+	case 250:
+		ans = 2;
+		break;
+	case 252:
+		ans = 3;
+		break;
+	case 226:
+		ans = 4;
+		break;
+	default:
+		ans = 0;
+		break;
+	}
+	return ans;
+}
+
+void debug(int val) {
+	wchar_t text_buffer[20] = { 0 }; // temporary buffer
+	swprintf(text_buffer, _countof(text_buffer), L"%d\n", (int)val); // convert
+	OutputDebugString(text_buffer); // print
 }
